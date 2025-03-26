13d2b48bba909285e3c66e25405bc62cd818bad7
navin <navin.tiwari.cse.2022@nist.edu>
2025-03-26 20:35:54 +0530

    hi3

---
+++ hi3/middleware/errorHandler.js
@@ -0,0 +1,42 @@
+const {constants} = require("../constants");
+const errorHandler = (err,req,res,next) => {
+        const statusCode =res.statusCode ? res.statusCode : 500;
+        switch (statusCode){
+            case constants.VALIDATION_ERROR: res.json({
+                    title: "validation failed" ,
+                    message: err.message,
+                    stackTrace: err.stack
+                });
+                break;
+            case constants.NOT_FOUND: res.json({
+                    title: "not found" ,
+                    message: err.message,
+                    stackTrace: err.stack
+                });
+                break;
+            case constants.FORBIDDEN: res.json({
+                    title: "FORBIDDEN" ,
+                    message: err.message,
+                    stackTrace: err.stack
+                });
+                break;
+            case constants.UNAUTHORIZED: res.json({
+                    title: "UNAUTHORIZED access" ,
+                    message: err.message,
+                    stackTrace: err.stack
+                });
+                break;
+            case constants.SERVER_ERROR: res.json({
+                title: "SERVER_ERROR" ,
+                message: err.message,
+                stackTrace: err.stack
+                });
+                break;
+            default:
+                console.log("No error,All good!");
+                break;
+        }
+
+};
+
+module.exports = errorHandler;
